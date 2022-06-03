eaf6a2df74bdf931b6dd572a541dbf6d065417ce
Giovanni Sce <giosce@verizon.net>
2022-06-03 16:34:42 -0400

    Initial commit.

---
+++ website/db-a.php
@@ -0,0 +1,20 @@
+<?php
+// debugging setting
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set("log_errors", 1);
+//ini_set("error_log", "/tmp/php-error.log");
+
+$properties = parse_ini_file("properties-a.ini");
+
+$host = $properties["host"];
+$username = $properties["username"];
+$password = $properties["password"];
+$db_name = $properties["db_name"];
+
+$conn = mysqli_connect($host, $username, $password, $db_name);
+if (!$conn) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
+}
+
+?>
---
+++ website/njcongress.php
@@ -0,0 +1,211 @@
+<html>
+<head>
+<title>BlueCompass.org</title>
+<script src="sorttable.js"></script>
+<style type="text/css">	
+table.sortable thead {
+    background-color:#eee;
+    color:#666666;
+    font-weight: bold;
+    cursor: default;
+}	
+
+.menu td:hover {
+	background-color: blue; 
+	color: white;
+	text-shadow: -.25px -.25px 0 white, .25px .25px white;
+	cursor: pointer;
+}
+
+.menu td {
+	text-decoration: none;
+	padding: 2px;
+	color: blue;
+}
+
+.aa {
+	text-decoration: none;
+	color: blue;
+	text-shadow: -.25px -.25px 0 transparent, .25px .25px transparent;	
+}
+.aa:hover {
+	color: white;
+	text-shadow: -.25px -.25px 0 white, .25px .25px white;
+}
+
+</style>
+
+<link rel="stylesheet" type="text/css" href="../dropdowntabfiles/ddcolortabs.css" />
+<script type="text/javascript" src="../dropdowntabfiles/dropdowntabs.js"></script>
+
+</head>
+
+<body style="font-family:Arial; font-size:small;">
+
+<div id="bluecompasstab" class="ddcolortabs">
+<ul>
+<li><a href="http://bluecompass.org" title="About Us, Memories" rel="dropmenu_home"><span>Home</span></a></li>
+<li><a href="http://bluecompass.org/elections" title="Registrations, predictions, past results and more" rel="dropmenu_elections"><span>Elections</span></a></li>
+<li><a href="http://bluecompass.org/cd" title="Congressional and Legislative Districts" rel="dropmenu_districts"><span>Districts</span></a></li>
+<li><a href="http://bluecompass.org/party" title="County Committees, Member roles and more" rel="dropmenu_party"><span>Democratic Party Information</span></a></li>
+<li><a href="http://bluecompass.org/myinfo" title="My District, my represantatives, my candidates"><span>My Information</span></a></li>
+<li><a href="http://bluecompass.org/projects" title="Build voter lists for voter outreach projects"><span>Voter Outreach</span></a></li>	
+</ul>
+</div>
+
+<br>
+
+<div id="dropmenu_home" class="dropmenudiv_a">
+<a href="http://bluecompass.org/aboutus.html">About Us</a>
+<a href="http://bluecompass.org/memories">Memories</a>
+<a href="http://bluecompass.org/forum">Forum</a>
+<a href="http://bluecompass.org/blog">Blog</a>
+</div>
+
+<div id="dropmenu_elections" class="dropmenudiv_a">
+<a href="http://bluecompass.org/2021">2021</a>
+<a href="http://bluecompass.org/2021/candidates.php">  2021 Candidates</a>
+<a href="http://bluecompass.org/2020">2020</a>
+<a href="http://bluecompass.org/2020/candidates.php">  2020 Candidates</a>
+<a href="http://bluecompass.org/njcongress.html">2019</a>
+<a href="http://bluecompass.org/congress.html">2018</a>
+</div>
+
+<div id="dropmenu_party" class="dropmenudiv_a">
+<a href="http://bluecompass.org/party/committees.php">County Committees</a>
+<a href="http://bluecompass.org/party/comm_seats.php">County Committees Seats</a>
+<a href="https://drive.google.com/open?id=1vwfg4734qTQkKakjXUHpDw4xHFd7ApzB">County Committee Member Role</a>
+<a href="http://bluecompass.org/party/party_survey.html">Survey</a>
+</div>
+
+<div id="dropmenu_districts" class="dropmenudiv_a">
+<a href="http://bluecompass.org/cd">Congressional Districts</a>
+<a href="http://bluecompass.org/ld">Legislative Districts</a>
+</div>
+
+<H2><center>New Jersey General Assembly and Senate</center></H2>
+
+<br>
+Every New Jersey legislative district is represented in the State Assembly by 2 Representatives and in the State Senate by 1 Senator.
+<BR>
+There are 40 Legislative Districts in NJ with roughly the same population each based on the last decennial census.
+For the 2021 elections, the districts have not been redesigned yet.
+
+<br>
+<br>
+<table border="1">
+<tr>
+  <td align="center"><b>2021 Legislature Seats</b></td>
+  <td align="center"><b>2021 Assembly Map</b></td>
+  <td align="center"><b>Districts</b></td>
+  <td align="center"><b>2021 Senate Map</b></td>
+</tr>  
+<tr>
+<td align="center" valign="top" style="padding-left:20px;padding-right:20px;padding-top:10px;">
+<a target="new" href="https://en.wikipedia.org/wiki/New_Jersey_Legislature">
+<img src="img/NJLegislatureSeats-2021.png" width="250" height="320" valign="top"></a>
+</td>
+<td align="center" style="padding-left:20px;padding-right:20px;padding-top:10px;" valign="top">
+<img src="img/800px-NJ_Assembly_map_2021.png" valign="top" width="280" height="500" usemap="#districtsmap">
+<map name="districtsmap">
+    <area target="" alt="LD01" title="LD01" href="ld/data?ld=01" coords="45,383,51,405,76,399,81,376,113,403,132,401,134,419,156,431,116,489,92,499,107,451,67,448,23,411,24,396" shape="poly">
+    <area target="" alt="LD14" title="LD14" href="ld/data?ld=14" coords="131,245,141,228,156,233,162,221,153,211,169,213,190,199,193,214,185,230,156,246,149,246,154,256,142,249" shape="poly">
+    <area target="" alt="LD06" title="LD06" href="ld/data?ld=06" coords="71,292,80,286,92,297,98,307,104,327,85,319,84,309,73,305,76,296" shape="poly">
+    <area target="" alt="LD09" title="LD09" href="ld/data?ld=09" coords="164,334,190,334,183,315,206,291,241,299,238,338,201,400,163,402,145,384,164,372,144,360,137,343,150,337,125,314,145,311" shape="poly">
+    <area target="" alt="LD23" title="LD23" href="ld/data?ld=23" coords="62,123,80,125,102,106,111,109,119,101,109,118,130,133,148,131,146,138,164,159,166,166,159,169,155,160,146,169,140,150,127,144,121,163,110,164,81,203,79,170,69,163,61,170,57,144,59,131" shape="poly">
+    <area target="" alt="LD38" title="LD38" href="ld/data?ld=38" coords="225,78,237,82,241,77,254,86,257,92,245,91,241,102,233,87,224,84" shape="poly">
+<!--
+    <area alt="LD24" title="LD24" href="leg_dist/ld24.php" coords="117,-1,61,69,68,110,103,91,123,101,131,74,171,27,120,-1" shape="poly">
+    <area alt="LD23" title="LD23" href="leg_dist/ld23.php" coords="55,114,103,94,98,111,128,123,152,152,108,136,72,181,72,147,49,150" shape="poly">
+    <area alt="LD21" title="LD21" href="ld/data?ld=21" coords="132,120,145,119,172,117,185,127,176,135,168,127,149,144,135,133" shape="poly">
+    <area alt="LD25" title="LD25" href="ld/data?ld=25.php" coords="139,119,131,114,121,117,100,107,108,95,122,104,128,95,124,85,133,80,146,88,161,73,168,84,148,95,162,109,146,108" shape="poly">	
+    <area target="" alt="LD01" title="LD01" href="ld/data?ld=01" coords="65,1190,137,1131,144,1202,226,1187,238,1115,321,1193,387,1193,444,1300,312,1481,262,1477,318,1337,199,1334,131,1268,74,1229" shape="poly">
+    <area target="" alt="LD03" title="LD03" href="ld/data?ld=03" coords="45,996,104,941,194,919,186,959,130,1004,180,1045,212,1022,301,1105,267,1140,233,1111,226,1179,151,1203,138,1128,63,1190,67,1225,14,1176,4,1075" shape="poly">
+    <area target="" alt="LD02" title="LD02" href="ld/data?ld=02" coords="269,1142,331,1076,360,1099,415,1038,488,1110,420,1139,492,1198,499,1187,543,1217,554,1192,585,1189,537,1238,479,1275,412,1260,389,1183,331,1194" shape="poly">
+    <area target="" alt="LD04" title="LD04" href="ld/data?ld=04" coords="206,1005,237,942,287,965,251,967,276,993,296,980,360,1044,304,1103" shape="poly">
+    <area target="" alt="LD05" title="LD05" href="ld/data?ld=05" coords="132,1003,196,941,200,873,227,877,214,909,257,929,220,946,220,965,181,1040" shape="poly">
+    <area target="" alt="LD06" title="LD06" href="ld/data?ld=06" coords="207,870,245,852,277,883,306,972,257,945,227,908" shape="poly">
+    <area target="" alt="LD09" title="LD09" href="ld/data?ld=09" coords="483,990,432,919,373,932,446,1001,393,1017,489,1112,424,1139,542,1213,588,1180,671,1065,708,892,611,860,538,935,558,987" shape="poly">
+    <area target="" alt="LD07" title="LD07" href="ld/data?ld=07" coords="236,849,348,769,410,735,415,764,405,776,389,770,375,777,375,807,354,802,358,822,338,827,321,852,333,852,330,899,285,899" shape="poly">
+    <area target="" alt="LD09" title="LD09" href="ld/data?ld=09" coords="330,900,329,841,362,807,390,767,452,813,429,837,445,850,501,851,560,988,486,991,431,918,374,929,389,957,417,966,443,1001,393,1017,414,1038,360,1097,331,1075,358,1045,299,981,265,987,257,969,295,968,310,970,287,900" shape="poly">
+    <area target="" alt="LD10" title="LD10" href="ld/data?ld=10" coords="510,865,625,810,668,832,677,784,695,765,721,816,707,890,611,860,538,935" shape="poly">
+    <area target="" alt="LD30" title="LD30" href="ld/data?ld=30" coords="624,809,623,775,636,766,620,751,619,720,642,687,687,692,688,711,725,731,745,719,731,781,718,784,720,803,712,806,696,762,677,785,668,831" shape="poly">
+    <area target="" alt="LD12" title="LD12" href="ld/data?ld=12" coords="452,730,477,727,561,669,580,637,569,604,606,561,614,570,627,558,639,564,632,578,643,581,639,597,631,591,593,629,614,652,592,704,550,741,617,746,633,767,620,772,623,809,507,864,502,850,446,850,430,836,451,812,410,777,419,742,462,765" shape="poly">
+    <area target="" alt="LD11" title="LD11" href="ld/data?ld=11" coords="551,741,590,704,608,664,640,672,645,630,689,649,713,627,725,627,733,660,760,643,748,716,725,730,687,709,687,691,643,685,618,720,619,745" shape="poly">
+    <area target="" alt="LD13" title="LD13" href="ld/data?ld=13" coords="612,661,614,651,594,628,631,590,639,594,644,578,636,570,685,563,750,590,746,542,765,589,760,644,735,657,723,624,689,646,646,628,639,671" shape="poly">
+    <area target="" alt="LD14" title="LD14" href="ld/data?ld=14" coords="452,619,507,640,536,610,545,615,565,593,577,641,558,672,530,688,513,686,515,694,486,711,475,730,449,729,462,765,387,729,410,680,460,696,480,656,450,640" shape="poly">
+    <area target="" alt="LD15" title="LD15" href="ld/data?ld=15" coords="295,614,324,571,342,573,371,543,418,658,449,636,479,657,460,695,410,678,386,727,343,687,326,651,310,644,295,631" shape="poly">
+    <area target="" alt="LD16" title="LD16" href="ld/data?ld=16" coords="327,488,344,488,369,469,367,453,380,433,386,450,399,440,429,466,419,505,458,486,471,508,475,554,443,592,455,617,470,600,471,586,496,574,544,600,507,640,456,618,448,635,418,656,370,541,343,572,325,568,295,612,249,594,302,527" shape="poly">
+    <area target="" alt="LD17" title="LD17" href="ld/data?ld=17" coords="474,513,537,481,555,513,533,532,557,543,543,568,514,585,496,572,473,587,456,617,445,591,475,554" shape="poly">
+    <area target="" alt="LD18" title="LD18" href="ld/data?ld=18" coords="538,477,585,471,593,514,589,544,574,549,570,561,583,578,571,594,540,614,544,599,519,584,538,567,557,544,536,531,555,512" shape="poly">
+    <area target="" alt="LD19" title="LD19" href="ld/data?ld=19" coords="587,471,601,469,610,478,651,472,645,498,630,506,619,537,626,559,614,568,605,560,587,581,569,558,593,541,594,515" shape="poly">
+    <area target="" alt="LD22" title="LD22" href="ld/data?ld=22" coords="562,426,585,458,615,446,637,440,649,449,650,472,611,476,599,468,532,481,499,499,501,483,518,461,533,458,552,447" shape="poly">
+    <area target="" alt="LD20" title="LD20" href="ld/data?ld=20" coords="612,396,645,406,651,418,670,416,688,428,673,447,657,441,651,446,638,439,621,445,613,437,629,430,613,420,599,418" shape="poly">
+    <area target="" alt="LD31" title="LD31" href="ld/data?ld=31" coords="727,364,732,372,719,389,733,395,745,389,742,405,721,421,714,446,674,448,710,393" shape="poly">
+    <area target="" alt="LD33" title="LD33" href="ld/data?ld=33" coords="742,355,752,359,744,385,733,392,721,387" shape="poly">
+    <area target="" alt="LD32" title="LD32" href="ld/data?ld=32" coords="686,357,710,364,731,340,752,327,767,332,756,359,738,355,732,365,721,370,700,403,699,380,681,386,676,374" shape="poly">
+    <area target="" alt="LD34" title="LD34" href="ld/data?ld=34" coords="669,286,689,289,694,298,685,301,682,314,694,325,690,331,664,315,651,349,663,365,654,383,631,374,646,352,639,344,655,309" shape="poly">
+    <area target="" alt="LD28" title="LD28" href="ld/data?ld=28" coords="664,317,690,329,688,341,670,340,654,385,660,397,646,410,629,398,638,384,650,382,664,365,652,347" shape="poly">
+    <area target="" alt="LD29" title="LD29" href="ld/data?ld=29" coords="670,343,687,344,675,377,677,387,696,382,701,413,686,425,670,414,651,415,650,409,658,394,656,383" shape="poly">
+    <area target="" alt="LD36" title="LD36" href="ld/data?ld=36" coords="696,299,721,315,730,299,768,322,762,329,751,327,725,342,714,365,681,356,694,322,683,312,685,302,692,303" shape="poly">
+    <area target="" alt="LD35" title="LD35" href="ld/data?ld=35" coords="654,233,665,233,670,258,681,260,695,270,708,293,704,304,687,286,655,281" shape="poly">
+    <area target="" alt="LD38" title="LD38" href="ld/data?ld=38" coords="668,230,704,246,707,232,750,244,750,258,762,259,760,272,735,271,726,275,720,294,724,301,719,311,708,303,708,290,683,258,670,255" shape="poly">
+    <area target="" alt="LD37" title="LD37" href="ld/data?ld=37" coords="771,205,799,216,790,281,768,328,721,295,729,271,763,276,763,244,781,256,788,222,774,218" shape="poly">
+    <area target="" alt="LD27" title="LD27" href="ld/data?ld=27" coords="526,329,550,324,562,332,588,319,590,327,606,332,614,319,638,338,643,352,630,372,638,379,626,402,613,394,604,402,564,377,554,403,536,417,532,402,504,415,485,377,487,356,519,371,536,366,542,346,520,340" shape="poly">
+    <area target="" alt="LD40" title="LD40" href="ld/data?ld=40" coords="615,208,629,223,654,194,671,206,681,190,693,193,690,205,712,217,712,230,704,231,702,243,668,230,654,236,651,276,667,287,645,324,632,319,637,304,593,250,590,224" shape="poly">
+    <area target="" alt="LD39" title="LD39" href="ld/data?ld=39" coords="615,121,770,202,767,209,789,221,779,252,762,242,762,255,751,256,744,240,714,232,713,217,692,205,694,190,677,188,671,206,655,192,629,220,615,207,592,226,573,211,582,171" shape="poly">
+    <area target="" alt="LD26" title="LD26" href="ld/data?ld=26" coords="569,94,615,120,583,169,571,212,588,225,600,261,646,320,638,337,613,317,605,333,585,321,562,330,544,320,519,332,495,324,525,295,539,301,563,284,550,248,532,259,490,298,436,252" shape="poly">	
+-->	
+</map>
+</td>
+<td valign="top" align="center" style='font-size:smaller;'>
+<?php
+for($i=1; $i<41; $i++) {
+	$a = $i;
+	if($a < 10) {
+		$a="0".$i;
+	}
+	if($i > 1) {
+		echo "<br>";
+	}
+	echo "<a href='ld/data?ld=".$a."'>LD".$a."</a>";
+}
+?>
+</td>
+<td valign="top" align="center" style="padding-left:20px;padding-right:20px;padding-top:10px;">
+<img src="img/800px-NJ_Senate_map_2021.png" width="280" height="500" valign="top">
+</td>
+</tr>
+</table>
+<br>
+<br>
+<form action="search_muni_ld.php">
+Find your district by town
+<input type="text" name="muni">
+<input type="submit" value="Submit">
+</form>
+<br>
+
+<br>
+<b>Tools</b>
+<ul>
+<li><a target="new" href="https://www.elec.state.nj.us/ELECReport/candidatemetrics.aspx">Donations to NJ lawmakers database</a></li>
+<li><a target="new" href="https://wwwnet-elec.state.nj.us/elec_ptp/AdvancedSearch.aspx">Pay-to-Play database</a></li>
+<li><a target="new" href="https://votesmart.org/officials/NJ/L/new-jersey-state-legislative#.YELSQGhKhPY">Vote Smart - NJ Legislators</a>
+</ul>
+<br>
+
+<b>Links</b>
+<ul>
+<li><a target="new" href="https://www.njelections.org/election-information.shtml">State of NJ Elections Website</a></li>
+<li><a target="new" href="http://ucnj.org/departments/clerk-of-the-board/freeholders/">The Union County Board of Chosen Freeholders</a></li>
+</ul>
+
+<br>
+</body>
+
+<script type="text/javascript">
+tabdropdown.init("bluecompasstab", 2);
+</script>
+
+</html>
